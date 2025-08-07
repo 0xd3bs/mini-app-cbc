51faecc3cd55a9056764ed201e32cc11deb10d28
0xd3bs <dblancove@gmail.com>
2025-08-07 22:20:26 +0000

    refactor(tokens): Update token images and available tokens

--- app/components/Components.tsx
+++ app/components/Components.tsx
@@ -12,7 +12,7 @@ const ETH_TOKEN: Token = {
   address: '', // Native token, no address
   symbol: 'ETH',
   decimals: 18,
-  image: 'https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png',
+  image: 'https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png',
   chainId: 8453,
 };
 
@@ -30,14 +30,13 @@ const USDC_TOKEN: Token = {
     address: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913', // Address for USDC on Base
     symbol: 'USDC',
     decimals: 6,
-    image: 'https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2',
+    image: 'https://dynamic-assets.coinbase.com/3c15df5e2ac7d4abbe9499ed9335041f00c620f28e8de2f93474a9f432058742cdf4674bd43f309e69778a26969372310135be97eb183d91c492154176d455b8/asset_icons/9d67b728b6c8f457717154b3a35f9ddc702eae7e76c4684ee39302c4d7fd0bb8.png',
     chainId: 8453,
 };
 
 const TOKEN_MAP: { [key: string]: Token } = {
   ETH: ETH_TOKEN,
-  WBTC: WBTC_TOKEN,
-  USDC: USDC_TOKEN,
+  //WBTC: WBTC_TOKEN,
 };
 
 type ButtonProps = {
